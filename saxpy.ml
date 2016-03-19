93a8028da8c2489c0d39ac52fba9db6f85c36bcd
Guillaume MICHEL <guillaume.michel@orilla.fr>
2016-03-19 22:53:01 +0100

    - Improved OCaml benchmark

--- saxpy.ml
+++ saxpy.ml
@@ -1,7 +1,7 @@
 open Core.Std
 open Sys
 
-let saxpy z a x y =
+let daxpy z a x y =
   for i = 0 to Array.length x - 1 do
     Array.set z i (a *. Array.get x i +. Array.get y i)
   done
@@ -9,18 +9,21 @@ let saxpy z a x y =
 let time f =
   let t = Unix.gettimeofday () in
   let fx = f () in
-  Printf.printf "execution elapsed time: %f sec\n"
-                (Unix.gettimeofday () -. t);
-  fx
+  (Unix.gettimeofday () -. t) *. 1000.0
 
 
-let bench_saxpy () =
-  let n = 100_000_000 in
-  let z = Array.create n 0.0 in
-  let a = 1.5 in
-  let x = Array.create n 5.3 in
-  let y = Array.create n 7.5 in
-  time (fun () -> saxpy z a x y)
+let bench n m f valx valy valz vala =
+  let z = Array.create n valz in
+  let a = vala in
+  let x = Array.create n valx in
+  let y = Array.create n valy in
+  time (fun () -> for i = 1 to m do
+                    f z a x y
+                  done)
 
 let () =
-  bench_saxpy ()
+  let n = [10; 100; 1_000; 10_000; 100_000; 1_000_000; 10_000_000; 100_000_000] in
+  let m = 10 in
+  List.iter ~f:(fun i -> let runtime = bench i m daxpy 1.5 3.7 0.0 1.5 in
+                         Printf.printf "%d, %f\n" i (runtime /. float_of_int m))
+            n
